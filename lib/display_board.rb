2f356dcf6566f171e98ae983ee03868ec22fcd34
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-04-29 22:44:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ def display_board(board=["X", "X", "X", " ", " ", " ", " ", " ", " "])
 
   puts(" #{board[0]} | #{board[1]} | #{board[2]} ")
   puts("-----------")
-  puts("   | X |   ")
+  puts(" #{board[3]} | #{board[4]} | #{board[5]} ")
   puts("-----------")
   puts("   |   |   ")
 end
